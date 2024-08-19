98e4d9202e42cbf71a89860f3e0b58e7915d4046
kyle <wilsonatuel422@gmail.com>
2024-08-19 17:33:52 +0800

    added env

--- src/main/java/com/weasle/test/service/DeviceService.java
+++ src/main/java/com/weasle/test/service/DeviceService.java
@@ -5,12 +5,9 @@ import org.springframework.stereotype.Service;
 import com.weasle.test.entity.Device;
 import com.weasle.test.repo.DeviceRepository;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 public class DeviceService {
